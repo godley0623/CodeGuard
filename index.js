7c5b1d30483205f4504af604c8f59c810ca9fc0e
godley0623 <godley11691@yahoo.com>
2023-10-05 19:05:36 -0400

    added input form for email address

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let passwords = localStorage.getItem('cg: pws')
 if (!passwords) passwords = {}
 
+let emailAddress = localStorage.getItem('cg: em')
+if (!emailAddress) emailAddress = ''
+
 const copyPassword = (host) => {
     if ( (host in passwords) === false ) {
         alert("You have no password generated for this website")
@@ -10,7 +13,7 @@ const copyPassword = (host) => {
     if (navigator.clipboard) {
         navigator.clipboard.writeText(passwords[host])
             .then(() => {
-                alert("Password has been copied")
+                alert(`The password for ${host} has been copied`)
             })
     } else {
         // Fallback for browsers that don't support the Clipboard API
@@ -20,9 +23,31 @@ const copyPassword = (host) => {
         textArea.select()
         document.execCommand("copy")
         document.body.removeChild(textArea)
-        alert("Password has been copied")
+        alert(`The password for ${host} has been copied`)
       }
 }
+
+const handleEmailBtn = () => {
+    if (!emailInput.value) return
+    if ( (emailInput.value.includes('@') === false) ) {
+        alert('Please enter a valid email address')
+        return
+    }
+
+    const response = window.confirm(`Are you sure you want to set your email address to ${emailInput.value}`)
+
+    if (!response) return
+
+    localStorage.setItem('cg: em', emailInput.value)
+    emailAddress = emailInput.value
+    alert(`Email Address set.\nAll passwords will be set to this email address: ${emailInput.value}`)
+    window.location.reload()
+}
+
+const emailInput = document.querySelector('.email-form-input')
+const emailButton = document.querySelector('.email-form-button')
+
+emailButton.addEventListener('click', handleEmailBtn)
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const activeTab = tabs[0].url
@@ -33,4 +58,12 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
     const copyPassBtn = document.querySelector(".copy-password")
     copyPassBtn.addEventListener("click", () => copyPassword(activeHost))
+
+    if (emailAddress) {
+        const passContainer = document.querySelector('.password-container')
+        passContainer.classList.remove('hidden')
+
+        const emailContainer = document.querySelector('.get-email')
+        emailContainer.classList.add('hidden')
+    }
 })
